cca8eef8e50922e2c2f7769706904b93cdc2a954
Godswill Udida <udidagodswill7@gmail.com>
2024-05-01 17:36:17 +0100

    pushed by udida

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+
+const port: number | undefined | string = process.env.PORT! || 33900;
+
+const app: Application = express();
+
+const server = app.listen(process.env.PORT! || port, () => {
+  console.log("Server is acive");
+});
+
+process.on("uncaughtException", (error: Error) => {
+  console.log(`uncaughtException: ${error.message}`);
+});
+
+process.on("unhandledRejection", (reason: Error | any) => {
+  console.log(`unhandledRejection: ${reason}`);
+});
+
+process.on("end", () => {
+  server.close();
+});
